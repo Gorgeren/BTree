22dab60c10155f3b36ac1c7bc302553a89ed89a2
Boris2 <Boris2@DESKTOP-AJ83CSU>
2022-05-21 16:22:36 +0300

    Добавьте файлы проекта.

---
+++ BTree/BTree.cpp
@@ -0,0 +1,55 @@
+﻿#include <vector>
+#include <iostream>
+
+class Key {
+public:
+    unsigned long long key;
+    std::string data;
+    Key(unsigned long long key, std::string data) : key(key), data(data) {};
+};
+class Node {
+public:
+    std::vector<Key*> keys;
+    std::vector<Node*> ptrs;
+    bool leaf;
+    Node(Key* key) : leaf(1) { keys.push_back(key); };
+    ~Node() {
+        for (Key* itr : keys) {
+            delete itr;
+        }
+        for (Node* itr : ptrs) {
+            delete itr;
+        }
+    }
+};
+class BTree {
+    Node* root;
+public:
+    void insert(unsigned long long key, std::string data);
+private:
+    void insertPrivate(unsigned long long key, std::string data);
+    Key* createKey(unsigned long long key, std::string data);
+    Node* createNode(unsigned long long key, std::string data);
+};
+Node* BTree::createNode(unsigned long long key, std::string data) {
+    Node *tmp = new
+}
+Key* BTree::createKey(unsigned long long key, std::string data) {
+    Key* tmp = new Key(key, data);
+    return tmp;
+}
+void BTree::insert(unsigned long long key, std::string data) {
+
+    if (root == nullptr) {
+        
+    }
+}
+void BTree::insertPrivate(unsigned long long key, std::string data) {
+
+}
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
+
